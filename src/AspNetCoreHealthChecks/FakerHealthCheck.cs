009c0dcf6e280c263fa7b52fbccd2833b85e226e
Cameron Cowen <cameronc94@gmail.com>
2025-02-25 23:26:52 +0000

    added initial health check blog sample

---
+++ src/AspNetCoreHealthChecks/FakerHealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AspNetCoreHealthChecks
+{
+    public class FakerHealthCheck(ILogger<FakerHealthCheck> logger, IFakerClient client) : IHealthCheck
+    {
+        private readonly IFakerClient _client = client;
+        private readonly ILogger<FakerHealthCheck> _logger = logger;
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _client.AddressesAsync();
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "FakerHealthCheck failed");
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
---
+++ src/AspNetCoreHealthChecks/Program.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCoreHealthChecks;
+using Microsoft.Extensions.Azure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient<IFakerClient, FakerClient>();
+
+builder.Services.AddAzureClients(configure =>
+{
+    configure.AddBlobServiceClient("UseDevelopmentStorage=true");
+});
+var healthCheckBuilder = builder.Services.AddHealthChecks()
+    .AddAzureBlobStorage()
+    .AddCheck<FakerHealthCheck>("faker");
+// Uncomment and to see unhealthy response
+//healthCheckBuilder.AddCheck<BrokenHealthCheck>("broken");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.MapHealthChecks("/healthz");
+
+await app.RunAsync();
